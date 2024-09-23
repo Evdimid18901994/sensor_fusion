e023d26bd7f80a8d1180a33680cf4eb1f0d08ba5
STARLINECOMP <evdimid@mail.ru>
2024-09-23 11:01:29 +0600

    commit

---
+++ sensor/DimaApp.py
@@ -0,0 +1,54 @@
+import streamlit as st
+from sensor.Pages import Brosay, Andreich, Gusmanych, Valentinich, Dima
+
+from streamlit_navigation_bar import st_navbar
+
+
+st.set_page_config(initial_sidebar_state="collapsed")
+
+pages = ["Дима","Андреич", 'Все бросайте', 'Гусманыч', 'Валентиныч']
+
+styles = {
+    "nav": {
+        "background-color": "rgb(123, 209, 146)",
+    },
+    "div": {
+        "max-width": "32rem",
+    },
+    "span": {
+        "border-radius": "0.5rem",
+        "color": "rgb(49, 51, 63)",
+        "margin": "0 0.125rem",
+        "padding": "0.4375rem 0.625rem",
+    },
+    "active": {
+        "background-color": "rgba(105, 114, 255, 0.25)",
+    },
+    "hover": {
+        "background-color": "rgba(255, 255, 255, 0.35)",
+    },
+}
+page = st_navbar(pages, styles = styles)
+
+if page == 'Дима':
+    Dima.Dima().app()
+
+elif page == "Андреич":
+    Andreich.Andreich().app()
+
+elif page == "Все бросайте":
+    Brosay.Brosay().app()
+
+elif page == "Гусманыч":
+    Gusmanych.Gus().app()
+
+elif page == "Валентиныч":
+    Valentinich.Vale().app()
+
+with st.sidebar:
+    st.write("Sidebar")
+
+
+
+
+
